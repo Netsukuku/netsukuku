d262a621ace03d55df2cad65cf0ff66dd0a83868
Alexander von Gluck IV <kallisti5@unixzen.com>
2014-09-14 11:09:41 -0500

    Build: Drop messy, poorly written autotools
    * Move back to a SCons build system
    * Place scons script at base of project

--- src/netsukuku.h
+++ src/netsukuku.h
@@ -22,11 +22,11 @@
 #include "config.h"
 #include "igs.h"
 
-#define VERSION_STR				"NetsukukuD "PACKAGE_VERSION
+#define VERSION_STR				"NetsukukuD "VERSION
 
 #ifdef DEBUG
 #undef VERSION_STR
-#define VERSION_STR				"NetsukukuD "PACKAGE_VERSION" (debug)"
+#define VERSION_STR				"NetsukukuD "VERSION" (debug)"
 #endif
 
 /*
