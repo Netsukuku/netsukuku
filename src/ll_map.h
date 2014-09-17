ba6c7a6bf9af8a1270388daad5bf56e50b3d8cba
Alexander von Gluck IV <kallisti5@unixzen.com>
2014-09-17 10:36:24 -0500

    headers: Fix missing header comments

--- src/ll_map.h
+++ src/ll_map.h
@@ -1,3 +1,17 @@
+/*
+ * ll_map.c
+ *
+ *		This program is free software; you can redistribute it and/or
+ *		modify it under the terms of the GNU General Public License
+ *		as published by the Free Software Foundation; either version
+ *		2 of the License, or (at your option) any later version.
+ *
+ * Authors:	Alexey Kuznetsov, <kuznet@ms2.inr.ac.ru>
+ *
+ *
+ * Alpt: Added ll_first_up_if
+ */
+
 #ifndef LL_MAP_H
 #define LL_MAP_H
 
--- src/ntkresolv.h
+++ src/ntkresolv.h
@@ -1,3 +1,21 @@
+/* This file is part of Netsukuku
+ *
+ * This source code is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation; either version 2 of the License,
+ * or (at your option) any later version.
+ *
+ * This source code is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * Please refer to the GNU Public License for more details.
+ *
+ * You should have received a copy of the GNU Public License along with
+ * this source code; if not, write to:
+ * Free Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ */
+
 #ifndef NTK_RESOLV_H
 #define NTK_RESOLV_H
 
