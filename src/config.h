e6bea6b2c01d6801ac0df94a615c9b4cd0ea0dde
MissValeska <valeska_grim@yahoo.com>
2014-10-16 05:31:13 -0700

    I fixed a compilation error, Dunno why a number in a #define wouldn't be in quotation marks, Also, Why is config.h in .gitignore?

---
+++ src/config.h
@@ -0,0 +1,110 @@
+/* src/config.h.  Generated from config.h.in by configure.  */
+/* src/config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* "Location of configuration files" */
+#define CONF_DIR "/etc/netsukuku"
+
+/* "Where the Netsukuku data is saved" */
+#define DATA_DIR "/usr/share/netsukuku"
+
+/* FreeBSD */
+/* #undef FREEBSD */
+
+/* GNU Linux */
+#define GNU_LINUX /**/
+
+/* Define to 1 if you have the <gmp.h> header file. */
+#define HAVE_GMP_H 1
+
+/* Define to 1 if you have the <inttypes.h> header file. */
+#define HAVE_INTTYPES_H 1
+
+/* Define to 1 if you have the `kvm' library (-lkvm). */
+/* #undef HAVE_LIBKVM */
+
+/* Define to 1 if you have the `nsl' library (-lnsl). */
+/* #undef HAVE_LIBNSL */
+
+/* Define to 1 if you have the `socket' library (-lsocket). */
+/* #undef HAVE_LIBSOCKET */
+
+/* Define to 1 if you have the `xnet' library (-lxnet). */
+/* #undef HAVE_LIBXNET */
+
+/* Define to 1 if you have the <memory.h> header file. */
+#define HAVE_MEMORY_H 1
+
+/* netlink */
+#define HAVE_NETLINK /**/
+
+/* NET_RT_IFLIST */
+/* #undef HAVE_NET_RT_IFLIST */
+
+/* Define to 1 if you have the <openssl/crypto.h> header file. */
+#define HAVE_OPENSSL_CRYPTO_H 1
+
+/* Define to 1 if you have the <pthread.h> header file. */
+#define HAVE_PTHREAD_H 1
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#define HAVE_STDINT_H 1
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+#define HAVE_STDLIB_H 1
+
+/* Define to 1 if you have the <strings.h> header file. */
+#define HAVE_STRINGS_H 1
+
+/* Define to 1 if you have the <string.h> header file. */
+#define HAVE_STRING_H 1
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#define HAVE_SYS_STAT_H 1
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#define HAVE_SYS_TYPES_H 1
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#define HAVE_UNISTD_H 1
+
+/* Define to 1 if you have the <zlib.h> header file. */
+#define HAVE_ZLIB_H 1
+
+/* IRIX 6.5 */
+/* #undef IRIX */
+
+/* OpenBSD */
+/* #undef OPEN_BSD */
+
+/* Name of package */
+#define PACKAGE "netsukuku"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT ""
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "netsukuku"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "netsukuku 0.0.9b"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "netsukuku"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "0.0.9b"
+
+/* ntkd.pid file location */
+#define PID_DIR "/var/run"
+
+/* Define to 1 if you have the ANSI C header files. */
+#define STDC_HEADERS 1
+
+/* SunOS 5 */
+/* #undef SUNOS */
+
+/* Version number of package */
+#define VERSION "0.0.9b"
